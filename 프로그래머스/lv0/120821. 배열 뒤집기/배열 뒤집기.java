ad53aca089bc4394f9495e8eb2ff19f55c0e5962
Yuka <135237200+yukada7@users.noreply.github.com>
2023-08-11 00:17:54 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        
+        for (int i = num_list.length - 1, j = 0; i >= 0; i--, j++) {
+            answer[j] = num_list[i];
+        }
+        
+        return answer;
+    }
+}
