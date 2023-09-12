1d24b1bba26c71d9a7eaf8fa780ddb2e34b77524
Yuka <135237200+yukada7@users.noreply.github.com>
2023-09-12 22:54:51 +0900

    [level 0] Title: 소인수분해, Time: 2.22 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        ArrayList<Integer> arr = new ArrayList<>();
+        int i = 2;
+        
+        while(n > 1) {
+            if(n % i == 0) {
+                arr.add(i);
+                n /= i;
+            } else {
+              i++;
+            }
+        }
+        
+        answer = arr.stream().distinct().mapToInt(Integer::intValue).toArray();
+        
+        return answer;
+    }
+}
