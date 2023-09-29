0f94e77ef276d54c4808900da9167320dd6852a1
Yuka <135237200+yukada7@users.noreply.github.com>
2023-09-29 23:47:29 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        
+        char[] arr = my_string.toCharArray();
+        
+        arr[num1] = my_string.charAt(num2);
+        arr[num2] = my_string.charAt(num1);
+        
+        answer = String.valueOf(arr);
+        
+        return answer;
+    }
+}
