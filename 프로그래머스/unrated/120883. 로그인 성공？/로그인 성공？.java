8ddef8b09402b5dc27b5e873340ac513061a76f0
Yuka <135237200+yukada7@users.noreply.github.com>
2023-10-22 22:41:11 +0900

    [level unrated] Title: 로그인 성공?, Time: 0.01 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "fail";
+        
+        for (int i = 0; i < db.length; i++) {
+            if (db[i][0].equals(id_pw[0])) {
+                if (db[i][1].equals(id_pw[1])) {
+                    answer = "login";
+                    
+                    return answer;
+                } else {
+                    answer = "wrong pw";
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
