6dca19faccbd8b0a240d8f3029b3136558930df1
Yuka <135237200+yukada7@users.noreply.github.com>
2023-11-05 13:49:15 +0900

    [level unrated] Title: 이어 붙인 수, Time: 1.11 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        String even = "";
+        String odd = "";
+        
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                even += Integer.toString(num_list[i]);
+            } else {
+                odd += Integer.toString(num_list[i]);
+            }
+        }
+        
+        return answer = Integer.parseInt(even) + Integer.parseInt(odd);
+    }
+}
