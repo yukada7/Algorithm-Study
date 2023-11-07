958d2984abf4ede56b979a21efe5d03b57d4e103
Yuka <135237200+yukada7@users.noreply.github.com>
2023-11-07 23:54:55 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 2.07 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        Arrays.fill(answer, -1);
+
+        for (int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+
+            int minGreaterValue = Integer.MAX_VALUE;
+
+            for (int j = s; j <= e; j++) {
+                if (arr[j] > k && arr[j] < minGreaterValue) {
+                    minGreaterValue = arr[j];
+                }
+            }
+
+            if (minGreaterValue != Integer.MAX_VALUE) {
+                answer[i] = minGreaterValue;
+            }
+        }
+        
+        return answer;
+    }
+}
