e2d753ceb030931b6609a64d62191708c61690bb
Yuka <135237200+yukada7@users.noreply.github.com>
2023-11-18 22:25:42 +0900

    [level unrated] Title: 9로 나눈 나머지, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        
+        int sum = 0;
+        
+        for (int i = 0; i < number.length(); i++) {
+            sum += number.charAt(i) - '0';
+        }
+        
+        return answer = sum % 9;
+    }
+}
