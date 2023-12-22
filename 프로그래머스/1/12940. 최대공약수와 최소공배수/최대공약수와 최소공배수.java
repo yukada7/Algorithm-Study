214ebce8b261092362fb459f4362bdf07a2884f4
Yuka <135237200+yukada7@users.noreply.github.com>
2023-12-23 01:16:01 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int max = Math.max(n, m);
+        int min = Math.min(n, m);
+        
+        while(min != 0) {
+            int r = max % min;
+            max = min;
+            min = r;
+        }
+        
+        int gcd = n * m / max;
+        
+        int[] answer = { max, gcd };
+        
+        return answer;
+    }
+}
