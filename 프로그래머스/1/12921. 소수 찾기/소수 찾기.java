fa6bfb0a7db84036c562ab92dd39d285253668cd
Yuka <135237200+yukada7@users.noreply.github.com>
2024-01-03 23:31:03 +0900

    [level 1] Title: 소수 찾기, Time: 189.61 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 2; i <= n; i++) {
+            boolean flag = true;
+            int a = (int) Math.sqrt(i);
+            
+            for (int j = 2; j <= a; j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag == true) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
