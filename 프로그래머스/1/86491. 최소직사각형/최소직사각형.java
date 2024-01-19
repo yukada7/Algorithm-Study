8f2b8573a3755e30fee49922a0275bc4cf97f596
Yuka <135237200+yukada7@users.noreply.github.com>
2024-01-20 01:54:59 +0900

    [level 1] Title: 최소직사각형, Time: 1.40 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int max_w = 0;
+        int max_h = 0;
+        
+        for(int i = 0; i < sizes.length; i++){
+            int w = Math.max(sizes[i][0], sizes[i][1]);
+            int h = Math.min(sizes[i][0], sizes[i][1]);
+            max_w = Math.max(max_w, w);
+            max_h = Math.max(max_h, h);
+        }
+        return answer = max_w * max_h;
+    }
+}
