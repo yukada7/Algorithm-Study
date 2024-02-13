35bd47b7e380bd37ac23debfb3a93936e0a7b819
Yuka <135237200+yukada7@users.noreply.github.com>
2024-02-13 23:54:19 +0900

    [level 0] Title: 카운트 다운, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start - end_num + 1];
+        
+        for(int i = 0; i <= start - end_num; i++){
+            answer[i] = start - i;
+        }
+        
+        return answer;
+    }
+}
