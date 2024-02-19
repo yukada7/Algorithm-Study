f5bcdfa8d1202f8da75d8df30a1b413fdc66f119
Yuka <135237200+yukada7@users.noreply.github.com>
2024-02-19 22:36:03 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        int cnt = 0;
+        
+        for (int i = n; i < num_list.length; i++) {
+            answer[cnt] = num_list[i];
+            cnt++;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            answer[cnt] = num_list[i];
+            cnt++;
+        }
+        
+        return answer;
+    }
+}
